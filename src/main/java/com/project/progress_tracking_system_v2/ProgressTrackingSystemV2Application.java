14bf2efdb452f01355cb87919ee003d967247fe7
faiq69280 <faiq69280@gmail.com>
2024-11-17 12:27:02 -0800

    initial commits

---
+++ src/main/java/com/project/progress_tracking_system_v2/ProgressTrackingSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.project.progress_tracking_system_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressTrackingSystemV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressTrackingSystemV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/progress_tracking_system_v2/ProgressTrackingSystemV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.progress_tracking_system_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackingSystemV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
